c4a7c78a11cc7a2f0587460a32d6d1f01d95b0ba
Feljohnny <felipijohnny@outlook.com>
2025-08-25 12:04:52 -0300

    ajustando babel

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,7 @@
+// babel.config.js
 module.exports = function (api) {
   api.cache(true);
-
   return {
-    presets: [
-      "babel-preset-expo",
-      [
-        "@babel/preset-env",
-        {
-          targets: {
-            node: "current",
-          },
-        },
-      ],
-      "@babel/preset-typescript",
-    ],
-    plugins: [],
+    presets: ["babel-preset-expo"],
   };
 };
